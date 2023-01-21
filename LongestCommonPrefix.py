2e39af76fa09665861fe6f1cb02b173c512cd040
Syed Inshal <bitf19a004@pucit.edu.pk>
2023-01-22 01:14:03 +0500

    leetcode-longestcommonprefix

---
+++ LongestCommonPrefix.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        minLen = sys.maxint
+        result = ""
+        for e in strs:
+            if len(e) < minLen:
+                minLen = len(e)
+        for i in range(0, minLen):
+            previous = strs[0][i]
+            for e in strs:
+                if e[i] != previous:
+                    if result is None: 
+                        return ""
+                    else:
+                        return result
+            result = result + previous
+        return result
